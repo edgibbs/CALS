984125963868df1d80c26587a11b5b1c4d68e2fd
Keith Lauher <klauher@cambriasolutions.com>
2017-07-14 13:47:28 -0700

    refactor other adults card

--- app/javascript/forms/OtherAdultsCard.js
+++ app/javascript/forms/OtherAdultsCard.js
@@ -1,33 +1,46 @@
+import Immutable from 'immutable'
 import React from 'react'
 import {OtherAdultsCardField} from '../common/OtherAdultsCardField'
+
+const defaults = {
+  otherAdultsComponents: [],
+  'insert': true,
+  otherAdultsComponentValue: 1,
+  nameField: {
+    firstName: '',
+    lastName: '',
+    middleName: ''
+  },
+  dateOfBirth: '',
+  availableApplicant: '',
+  relationshipType: '',
+  relationshipTypes: {
+    items: []
+  },
+  availableApplicants: {
+    items: []
+  }
+}
 
 export default class OtherAdultsCard extends React.Component {
   constructor (props) {
     super(props)
     this.addCard = this.addCard.bind(this)
     this.removeCard = this.removeCard.bind(this)
+    this.handleNameFieldInput = this.handleNameFieldInput.bind(this)
+    this.onFieldChange = this.onFieldChange.bind(this)
+    this.clickClose = this.clickClose.bind(this)
     this.state = {
-      otherAdultsComponents: [],
-      'insert': true,
-      otherAdultsComponentValue: 1
+      otherAdults: [defaults]
     }
-    this.state.otherAdultsComponents.push(<OtherAdultsCardField {... this} id={this.state.otherAdultsComponentValue} key={this.state.otherAdultsComponentValue} />)
-  }
-
-  getInitialState () {
-    this.state.otherAdultsComponents = (<OtherAdultsCardField />)
   }
 
   addCard (event) {
+    let otherAdultsList = this.state.otherAdults
+    otherAdultsList.push(defaults)
     this.setState({
-      'insert': true
+      otherAdults: otherAdultsList
     })
-    if (this.state.insert) {
-      this.setState({
-        'otherAdultsComponentValue': this.state.otherAdultsComponentValue += 1
-      })
-      this.state.otherAdultsComponents.push(<OtherAdultsCardField {... this} id={this.state.otherAdultsComponentValue} key={this.state.otherAdultsComponentValue} />)
-    }
   }
 
   removeCard (value) {
@@ -39,10 +52,52 @@ export default class OtherAdultsCard extends React.Component {
     })
   }
 
+  clickClose (id) {
+    if (id === 1) {
+      this.setState(defaults)
+    } else {
+      this.props.removeCard(id)
+    }
+  }
+
+  onFieldChange (index, value, type) {
+    let otherAdultsList = Immutable.fromJS(this.state.otherAdults)
+    if (otherAdultsList.size === 0) {
+      otherAdultsList = otherAdultsList.push(defaults)
+    }
+    otherAdultsList = otherAdultsList.update(index, x => x.set(type, value))
+    this.setState({
+      // convert to regular js array
+      otherAdults: otherAdultsList.toJS()
+    })
+  }
+
+  handleNameFieldInput (index, value, type) {
+    let otherAdultsList = Immutable.fromJS(this.state.otherAdults)
+    otherAdultsList = otherAdultsList.update(index,
+       otherAdult => otherAdult.setIn(['nameField', type], value))
+    this.setState({
+      otherAdults: otherAdultsList.toJS()
+    })
+  }
+
   render () {
+    let otherAdultsList = this.state.otherAdults
     return (
       <div className='card-body'>
-        {this.state.otherAdultsComponents}
+        {
+          otherAdultsList.map((otherAdultsFields, index) => {
+            return (
+              <OtherAdultsCardField
+                key={index}
+                value={index}
+                relationshipTypes={this.props.relationshipTypes}
+                otherAdults={otherAdultsFields}
+                handleNameFieldInput={this.handleNameFieldInput}
+                onFieldChange={this.onFieldChange} />
+            )
+          }
+        )}
         <div className='text-center'>
           <button onClick={this.addCard} className='btn btn-default'>Add another Adult +</button>
         </div>
